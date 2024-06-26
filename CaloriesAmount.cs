98bb9b94b30dccef3f39abfe3fdbc3f2dc7ed9e1
priya <ST10072500@vcconnect.edu.za>
2024-06-27 01:51:09 +0200

    Created the CaloriesAmount class for POE_Part 3 of the WPF application

---
+++ CaloriesAmount.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PROG_6221_Part_3_POE_ST10072500
+{
+    public class CaloriesAmount
+    {
+    }
+}
