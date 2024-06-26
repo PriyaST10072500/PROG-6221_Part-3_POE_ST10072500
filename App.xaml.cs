1f6250774db3dc73b72ab45de6e98e21f195158a
priya <ST10072500@vcconnect.edu.za>
2024-06-27 01:24:17 +0200

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace PROG_6221_Part_3_POE_ST10072500
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
